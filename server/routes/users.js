248089744d86a06e87b25858031744d33224eddf
Jan <jan.evseev@gmail.com>
2016-10-13 21:17:21 +0300

    saves user in psql db

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,14 +1,23 @@
 import express from 'express';
 import validateInput from '../shared/validations/signup';
 let router = express.Router();
+import bcrypt from 'bcryptjs';
 
+import User from '../models/User';
 
 
 router.post('/', (req, res) => {
   const { errors, isValid } = validateInput(req.body);
 
   if (isValid) {
-    res.json({ success: true })
+    const { username, password, email } = req.body;
+    const password_digest = bcrypt.hashSync(password, 10);
+
+    User.forge({
+      username, email, password_digest
+    }, { hasTimestamps: true }).save()
+      .then(user => res.json({ success:true }))
+      .catch(err => res.status(500).json({ error: err }));
   } else {
     res.status(400).json(errors);
   }
