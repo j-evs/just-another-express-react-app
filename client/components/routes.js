0edfd099bddae77df58374a04b8f0c427e80f6b3
Jan <jan.evseev@gmail.com>
2016-10-25 23:54:27 +0300

    Login form

--- client/components/routes.js
+++ client/components/routes.js
@@ -4,10 +4,12 @@ import { Route, IndexRoute } from 'react-router';
 import App from './App';
 import Greetings from './Greetings';
 import SignupPage from './signup/SignupPage';
+import LoginPage from './login/LoginPage';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={Greetings} />
     <Route path="signup" component={SignupPage} />
+    <Route path="login" component={LoginPage} />
   </Route>
 )
